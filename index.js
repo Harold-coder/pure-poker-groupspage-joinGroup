4a3eeb32e9b2715d2b84c34139d277036778aecb
Harold-coder <hjc2154@columbia.edu>
2024-03-06 18:10:20 -0500

    Code Build set up

---
+++ index.js
@@ -0,0 +1,51 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const groupsTableName = process.env.GROUPS_TABLE; // Make sure this environment variable is set to your DynamoDB table name
+
+exports.handler = async (event) => {
+    const { groupId, userId } = JSON.parse(event.body);
+
+    try {
+        // Retrieve the current state of the group, including its maxMembers attribute
+        const groupData = await dynamoDb.get({
+            TableName: groupsTableName,
+            Key: { groupId },
+        }).promise();
+
+        let group = groupData.Item;
+        if (!group) {
+            // Group not found; optionally handle group creation or return an error
+            return { statusCode: 404, body: JSON.stringify({ message: "Group not found.", action: 'joinGroup' }) };
+        }
+
+        // Ensure the group does not exceed the maximum number of members
+        const maxMembers = group.maxMembers || Infinity; // Fallback to Infinity if maxMembers is not set, effectively no limit
+        if (group.members && group.members.length >= maxMembers) {
+            return { statusCode: 400, body: JSON.stringify({ message: "Group has reached its maximum number of members.", action: 'joinGroup' }) };
+        }
+
+        // Check if the user is already a member of the group
+        if (!group.members || !group.members.includes(userId)) {
+            // Add the user to the members array
+            const newMembers = group.members ? [...group.members, userId] : [userId];
+
+            // Update the group in the database to include the new member
+            await dynamoDb.update({
+                TableName: groupsTableName,
+                Key: { groupId },
+                UpdateExpression: "SET members = :newMembers",
+                ExpressionAttributeValues: {
+                    ":newMembers": newMembers,
+                },
+            }).promise();
+
+            return { statusCode: 200, body: JSON.stringify({ message: 'User joined the group successfully.', action: 'joinGroup' }) };
+        } else {
+            // User is already a member of the group
+            return { statusCode: 200, body: JSON.stringify({ message: 'User is already a member of the group.', action: 'joinGroup' }) };
+        }
+    } catch (err) {
+        console.error('Error joining group:', err);
+        return { statusCode: 500, body: JSON.stringify({ message: 'Failed to join group', action: 'joinGroup' }) };
+    }
+};
